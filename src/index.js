cda05cff143fbfea62d19a5c0dc28b7dcdb51cdd
turnerdylan <dytu4626@colorado.edu>
2021-08-25 10:19:14 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const Beholder = window['beholder-detection'];
 
+var canvas;
 //create game elements
 var wall = new Wall(new Vec2(60, 70), new Vec2(90, 70), new Vec2(60, 220), new Vec2(90, 220));
 var wall2 = new Wall(new Vec2(660, 70), new Vec2(690, 70), new Vec2(660, 220), new Vec2(690, 220));
-const ball = new Ball(350, 175);
+const ball = new Ball(812/2, 175);
 
 //0 = start, 1 = playing
 var gamestate = 0;
@@ -106,18 +107,9 @@ function keyDownHandler(e) {
     wall2.translate(new Vec2(50, 0));
   }
 
-  // if (e.key == "w") {
-  //   wall.translateTo(new Vec2(100, 100));
-  // }
-  // if (e.key == "a") {
-  //   wall.translateTo(new Vec2(200, 200));
-  // }
-  // if (e.key == "s") {
-  //   wall.translateTo(new Vec2(300, 300));
-  // }
-  // if (e.key == "d") {
-  //   wall.translateTo(new Vec2(50, 0));
-  // }
+  if (e.key == "g") {
+    StartGame();
+  }
 }
 
 function drawLine() {
@@ -133,11 +125,14 @@ function drawLine() {
 function drawText()
 {
   ctx.font = '35px serif';
-  ctx.fillText("Cyber-Hockey", canvas.width / 2, canvas.height / 2);
+  ctx.fillText("Cyber-Hockey", canvas.width / 2, canvas.height * 0.4);
 
   ctx.font = '25px serif';
-  ctx.fillText("Move your paddles onto the red squares to start", canvas.width / 2, (canvas.height / 2) + 30);
+  ctx.fillText("Move your paddles onto the red squares to start", canvas.width / 2, (canvas.height / 2));
+}
 
+function drawScores()
+{
   ctx.font = '30px serif';
   ctx.fillText(player1score, canvas.width / 2 - 30, 40);
   ctx.fillText(player2score, canvas.width / 2 + 30, 40);
@@ -159,7 +154,7 @@ function CheckMarkerLocations()
   var Ydiff = wall.center.y - (canvas.height / 2);
   var dist = Math.sqrt(Xdiff*Xdiff + Ydiff*Ydiff)
 
-  var Xdiff2 = wall2.center.x - (canvas.width * 0.8 + 30);
+  var Xdiff2 = wall2.center.x - (canvas.width * 0.85 + 30);
   var Ydiff2 = wall2.center.y - (canvas.height / 2);
   var dist2 = Math.sqrt(Xdiff*Xdiff + Ydiff*Ydiff)
 
@@ -182,24 +177,12 @@ function update() {
   //beholder update
   Beholder.update();
 
-  //get marker objects
-  // if(!playerMarkerNum1.present) console.log('missing 1');
-  // if(!playerMarkerNum2.present) console.log('missing 2');
-  // if(!referenceMarker.present) console.log('missing ref');
-
   if(gamestate == 0)
   {
     if(CheckMarkerLocations() == true)
     {
-      gamestate = 1;
+     StartGame();
     }
-    //if markers are in right location
-    //change gamestate to 1
-  }
-  else if (gamestate == 1)
-  {
-     //delete text and squares
-     //let ball update
   }
 
 
@@ -244,17 +227,29 @@ function update() {
   requestAnimationFrame(update);
 }
 
+function StartGame()
+{
+    gamestate = 1;
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  
   ctx.lineCap = "round";
-  drawLine();
-  drawText();
-  drawSquares();
 
+  //always draw
+  drawLine();
+  drawScores();
   wall.draw(ctx);
   wall2.draw(ctx);
-  if(gamestate == 1)
+
+  //conditional draw
+  if(gamestate == 0)
+  {
+    drawText();
+    
+    drawSquares();
+  }
+  else if(gamestate == 1)
   {
     ball.draw(ctx);
   }
